e8dd5439d98f404b6e65da779e5e5cb7f5bac110
Simran3611 <98481310+Simran3611@users.noreply.github.com>
2022-02-28 22:42:12 -0600

    Create Opcode.java

---
+++ Opcode.java
@@ -0,0 +1,18 @@
+public enum Opcode {
+    ADDI,
+    SW,
+    SLL,
+    LW,
+    SRL,
+    ADD,
+    SUB,
+    MUL,
+    MOVZ,
+    BREAK,
+    NOP,
+    JR,
+    J,
+    INVALID,
+    BLTZ,
+    ERROR
+}
